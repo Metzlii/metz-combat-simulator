41b3d0179d56e6a7ba6f277fabdf8da7be9bde13
Metzli <cbwallen13work@gmail.com>
2026-09-21 23:51:17 -0600

    Release: five finalists by default, the count a setting (source 36c0ab0)

--- zone-web/zoneProbeWorker.js
+++ zone-web/zoneProbeWorker.js
@@ -1,1 +1,1 @@
-import{burnN as e}from"./zoneProbe.js?v=c6998b526bf5";onmessage=n=>{const o=Number(n.data);e(o);const r=performance.now();e(o),postMessage(performance.now()-r)};
+import{burnN as e}from"./zoneProbe.js?v=1f9074f770c5";onmessage=n=>{const o=Number(n.data);e(o);const r=performance.now();e(o),postMessage(performance.now()-r)};
