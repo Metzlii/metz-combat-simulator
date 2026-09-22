caeb6111fa42d9e5114abbed34defa52d7c2e7f4
Metzli <cbwallen13work@gmail.com>
2026-09-21 22:28:01 -0600

    Release: editor opens on the first seated player, editing tag, Party Export 0.5 (source aae3303)

--- zone-web/zoneProbeWorker.js
+++ zone-web/zoneProbeWorker.js
@@ -1,1 +1,1 @@
-import{burnN as e}from"./zoneProbe.js?v=c29471879551";onmessage=n=>{const o=Number(n.data);e(o);const r=performance.now();e(o),postMessage(performance.now()-r)};
+import{burnN as e}from"./zoneProbe.js?v=f5d70b11a40c";onmessage=n=>{const o=Number(n.data);e(o);const r=performance.now();e(o),postMessage(performance.now()-r)};
